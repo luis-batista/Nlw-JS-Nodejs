3914197525eb6ba86efe85ef19fc23510f967da2
luis-batista <110860952+luis-batista@users.noreply.github.com>
2024-09-11 20:26:52 -0300

    add npm install inquirer

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 function start() {
 let count = 1
-    while(count <= 10){
-        console.log(count)
-        count++
+    while(true){
+        let opcao = 'sair'
+        switch(opcao) {
+            case 'cadastrar':
+                console.log('cadastre aqui')
+                break
+            case 'listar':
+                console.log('liste aqui')
+                break
+            case 'sair':
+                return
+
+        }
+
     }
 }
-
 start()
