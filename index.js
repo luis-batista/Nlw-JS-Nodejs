d6fd729908416c03717ae4235e9ec89366318fe8
luis-batista <110860952+luis-batista@users.noreply.github.com>
2024-09-11 20:00:15 -0300

    inicio

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = { 
+    value: 'deitar cedo',
+    checked: true
+}
+
+let metas = [
+    meta,
+    {
+        value: 'caminhar todos os dias',
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
