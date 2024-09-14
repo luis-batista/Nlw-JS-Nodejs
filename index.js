ced416926fd4c55ba424144fed39eaeceb248f21
luis-batista <110860952+luis-batista@users.noreply.github.com>
2024-09-14 11:41:17 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {select, input} = require('@inquirer/prompts') 
+const {select, input, checkbox} = require('@inquirer/prompts') 
 let meta = {
     value: "Tomar 3L de água",
     checked: false
@@ -6,6 +6,33 @@ let meta = {
 let metas = [
     meta
 ]
+
+async function listarMetas() {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false
+    })
+
+    if(respostas.length == 0){
+        console.log("Nenhuma meta seledionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) =>{
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log("Meta(s) marcadas como concluída(s)")
+}
 
 async function cadastrarMeta() {
     const meta = await input({message: "Digite a meta: "})
@@ -17,7 +44,8 @@ async function cadastrarMeta() {
 
     metas.push(
         {
-            value: meta, checked: false
+            value: meta, 
+            checked: false
         }
     )
 
@@ -55,7 +83,7 @@ async function start() {
                 console.log(metas)
                 break
             case 'listar':
-                console.log('liste aqui')
+                await listarMetas()
                 break
             case 'sair':
                 console.log("Até a próxima!")
